2d25946f5ce109201cdb96f39900016f9332bd31
Maksym Moroz <maxmorozadult1@gmail.com>
2026-05-26 17:19:59 +0200

    production11.11

--- src/main/java/hr/algebra/javawebprj/config/RailwayDatabaseEnvironmentPostProcessor.java
+++ src/main/java/hr/algebra/javawebprj/config/RailwayDatabaseEnvironmentPostProcessor.java
@@ -1,7 +1,7 @@
 package hr.algebra.javawebprj.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 public class RailwayDatabaseEnvironmentPostProcessor implements EnvironmentPostProcessor {
